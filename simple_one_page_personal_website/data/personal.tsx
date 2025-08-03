95d59f2f3181a0be4f3324bb9988f8b7698d6947
Key666777 <fang022166@gmail.com>
2025-08-02 17:23:07 -0700

    me

--- simple_one_page_personal_website/data/personal.tsx
+++ simple_one_page_personal_website/data/personal.tsx
@@ -2,10 +2,8 @@
 
 const firstName = "Fred";
 const lastName = "Fang";
-const personalSlogan =
-  "Building amazing digital experiences, one project at a time.";
-const heroSectionText = `A passionate high school student who loves creating 
-                        innovative solutions and building amazing digital 
-                        experiences. Specialized in [Your Specialization].`;
+const personalSlogan = "Building amazing digital experiences, one project at a time.";
+const heroSectionText = `A passionate high school student who loves creating
+innovative solutions and building amazing digital experiences. Specialized in [Your Specialization].`;
 
 export { firstName, lastName, personalSlogan, heroSectionText };
